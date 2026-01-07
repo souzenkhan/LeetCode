b66afabfc3c5dff53f9a133ae2d0cccc0ac34048
Souzen Khan <115444792+souzenkhan@users.noreply.github.com>
2026-01-06 22:05:24 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (50.29%) - LeetSync

--- Linked-List/866-rectangle-overlap/rectangle-overlap.py
+++ Linked-List/866-rectangle-overlap/rectangle-overlap.py
@@ -6,20 +6,10 @@ class Solution(object):
         :rtype: bool
         """
 
-        overlap_left = max(rec1[0], rec2[0])
-        overlap_right = min(rec1[2], rec2[2])
-        overlap_top = min(rec1[3], rec2[3])
-        overlap_bottom = max(rec1[1], rec2[1])
+        if(rec1[0] == rec1[2] or rec1[1] == rec1[3] or rec2[0] == rec2[2] or rec2[1] == rec2[3]):
+            return False
 
-        overlap_width = overlap_right - overlap_left
-        overlap_height = overlap_top - overlap_bottom
-
-        overlaps = False
-
-        if(overlap_width > 0 and overlap_height > 0):
-            overlap_area = overlap_width * overlap_height
-            if overlap_area > 0: 
-                overlaps = True 
-
-
-        return overlaps
+        return not(rec1[2] <= rec2[0] or #left
+                    rec1[3] <= rec2[1] or #bottom
+                    rec1[0] >= rec2[2] or #right
+                    rec1[1] >= rec2[3]) #top
