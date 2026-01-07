e73a8cc4837da5e8be53ddc3b56daa5067e188bf
Souzen Khan <115444792+souzenkhan@users.noreply.github.com>
2026-01-06 22:17:26 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.87%) - LeetSync

--- Linked-List/866-rectangle-overlap/rectangle-overlap.py
+++ Linked-List/866-rectangle-overlap/rectangle-overlap.py
@@ -6,10 +6,14 @@ class Solution(object):
         :rtype: bool
         """
 
-        if(rec1[0] == rec1[2] or rec1[1] == rec1[3] or rec2[0] == rec2[2] or rec2[1] == rec2[3]):
+        if(rec1[0] == rec1[2] or 
+            rec1[1] == rec1[3] or 
+            rec2[0] == rec2[2] or
+            rec2[1] == rec2[3]):
             return False
-
+        #x1 y1 x2 y2
+        #0.  1. 2  3
         return not(rec1[2] <= rec2[0] or #left
-                    rec1[3] <= rec2[1] or #bottom
+                    rec1[3] <= rec2[1] or #top
                     rec1[0] >= rec2[2] or #right
-                    rec1[1] >= rec2[3]) #top
+                    rec1[1] >= rec2[3])
